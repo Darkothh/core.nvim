9ce82c48eec4a31bdb041c492b1ea0d12a869d4d
Dylan Fierro <fierrodylan34@gmail.com>
2022-09-07 13:38:11 -0600

    update

--- lua/server/init.lua
+++ lua/server/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-if pcall(require, "notify") then
+if pcall(require, "notifyy") then
   function Notify(text, type, opts)
     Plugin = 'LiveServer.nvim'
     require("notify")(text, type, { title = Plugin, opts })
@@ -15,7 +15,7 @@ M.verifi = false
 
 function M.verif()
   if pcall(require, "toggleterm") then
-   M.verifi = true
+    M.verifi = true
   else
     Notify('Core need ToogleTerm to work, pls install it ( https://github.com/akinsho/toggleterm.nvim )')
   end
@@ -25,36 +25,114 @@ M.verif()
 
 M.current = nil
 
-function M.server()
-  if M.verifi == false then
-    Notify('server = false')
+local default = {
+  port = 'auto',
+  host = 'auto',
+  browser = 'auto',
+  no_browser = false,
+}
+
+local liveServer = "live-server "
+local Port = nil
+local Host = nil
+local Browser = nil
+local No_browser = ""
+-- local Pconf = false
+
+
+
+-- P(default)
+
+-- file = vim.fn.expand("%:p")
+function M.setup(opts)
+  if opts then
+    opts = opts
+    -- Pconf = true
   else
-    local Terminal = require('toggleterm.terminal').Terminal
-    if M.current == nil then
-      M.current = Terminal:new({
-        cmd = "live-server",
-        dir = '%:p:h',
-        hidden = true,
-        on_open = function() Terminal:set_mode("n")  end,
-        on_exit = function() Notify('Consola Cerrada', 'error') end
-      })
-      Notify('Consola Iniciada', 'info')
-      M.current:toggle()
-    else
-      M.current:toggle()
-    end
+    opts = default
   end
+
+  P(opts)
+
+  local function getOpts()
+    -- if Pconf == true then
+    if opts.browser == 'auto' then
+      print("Browser default")
+      Browser = ""
+    elseif opts.browser ==  nil then
+      print("Browser default 0.0.0.0")
+      -- return
+    else
+      Browser = "--browser=" .. opts.browser .. " "
+    end
+    if opts.port == nil then
+      print("Port default")
+      Port = ""
+    elseif opts.port == '8080' or "auto" then
+      print("Port Default 8080")
+      Port = ""
+    else
+      Port = "--port=" .. opts.port .. " "
+    end
+    if opts.no_browser == true then
+      No_browser = "--no-browser "
+    else
+      print("No browser")
+      No_browser = ""
+    end
+    if Host == nil then
+      print("nil host")
+      if opts.host == 'auto' then
+        print('auto host')
+        Host = ""
+      elseif opts.host == '0.0.0.0' then
+        print("0 host")
+        Host = ""
+      else
+        Host = "--host=" .. opts.host .. " "
+      end
+    end
+
+    Prueba = liveServer .. Host .. No_browser .. Port .. Browser
+    -- else
+    -- Prueba = liveServer
+    -- print("Pconf: False")
+    -- end
+  end
+
+  getOpts()
+
+  ComandoFinal = Prueba
+  -- print("a"..Host)
+  print("comando final " .. "'" .. ComandoFinal .. "'")
+
+  -- local Terminal = require('toggleterm.terminal').Terminal
+  -- if M.current == nil then
+  --   M.current = Terminal:new({
+  --     dir = "%:p:h",
+  --     cmd = ComandoFinal,
+  --     hidden = true,
+  --     on_open = function() Terminal:set_mode("n") end,
+  --     on_exit = function() Notify('Server Closed', 'error') end
+  --   })
+  --   Notify('Server Start', 'info')
+  --   M.current:toggle()
+  -- else
+  --   M.current:toggle()
+  -- end
 end
 
+M.setup()
+
 function M.server_toggle()
-if M.verifi == false then
-  Notify('toggle == false')
-else
-  if M.current == nil then
-    M.server()
+  if M.verifi == false then
+    Notify('toggle == false')
+  else
+    if M.current == nil then
+      M.setup()
+    end
+    M.current:toggle()
   end
-  M.current:toggle()
-end
 end
 
 return M
--- plugin/server.lua
+++ plugin/server.lua
@@ -1,8 +1,2 @@
-vim.cmd "command! ServerRun lua require('server').server()"
+vim.cmd "command! ServerRun lua require('server').setup()"
 vim.cmd "command! ServerToggle lua require('server').server_toggle()"
-
--- local function map(mode, shortcut, command)
---   vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
--- end
--- map('n', '<leader>t', ':ServerToggle<cr>')
--- map('n', '<leader>g', ':ServerRun<cr>')
